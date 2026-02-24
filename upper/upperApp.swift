fe6957cd4301e53375009ccf5f5eae5348370424
edu-wmv <edu_monteiro1@outlook.com>
2026-02-24 15:44:24 -0300

    Initial Commit

---
+++ upper/upperApp.swift
@@ -0,0 +1,17 @@
+//
+//  upperApp.swift
+//  upper
+//
+//  Created by Eduardo Monteiro on 24/02/26.
+//
+
+import SwiftUI
+
+@main
+struct upperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
